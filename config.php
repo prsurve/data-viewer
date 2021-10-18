7796a94154aac050970a3549480e05ffdb9139f8
Pratik Surve <prsurve@redhat.com>
2021-10-18 12:24:23 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_NAME', 'mydatabase');
+define('DB_USER', 'root');
+define('DB_PASSWORD', $_ENV["DB_PASS"]);
+define('DB_HOST', 'mysql');
+
+// Create connection
+$db		=	new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+// Check connection
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+?>
