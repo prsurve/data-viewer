0f8ec6e58099d478ad0afbaf48ab30a2151f22c6
Pratik Surve <prsurve@redhat.com>
2021-10-18 12:27:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,78 @@
+<?php include_once('config.php'); include_once('paginator.class.php'); ?>
+<!DOCTYPE html>
 <html>
-<head><title> Display.php </title></head>
-<body bgcolor="aabbcc">
-<?php
-
-
-define('DB_SERVER', 'mysql');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'password');
-define('DB_DATABASE', 'mydatabase'); //where customers is the database
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-
-
-$query="select * from WORKLOAD"; // Fetch all the records from the table address
-$result=mysqli_query($db,$query);
-?>
-
-<h3> Page to display the stored data </h3>
-
-<table border="1">
-<tr>
-<th> DATE </th>
-<th> DATA  </th>
-
-
-<?php while($array=mysqli_fetch_row($result)) ?>
-<tr>
-<td><?echo $array[0];?></td>
-<td><?echo $array[1];?></td>
-</tr>
-
-<?php endwhile; ?>
-<?php mysqli_free_result($result); ?>
-<?php mysqli_close($db); ?>
-</table>
+<head>
+<title>Pagination with bootstrap</title>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	
+	<?php
+	if(isset($_REQUEST['tb1'])) {
+		$condition		=	"";
+				
+		$qryStr		=	"SELECT * FROM WORKLOAD"; 
+		$workload_data	=	$db->query($qryStr);
+		$count		=	$workload_data->num_rows;
+		
+		$pages = new Paginator($count,9);
+		echo '<div class="col-sm-6">';
+		echo '<nav aria-label="Page navigation"><ul class="pagination">';
+		echo $pages->display_pages();
+		echo '</ul></nav>';
+		echo '</div>';
+		echo '<div class="col-sm-6 text-right">';
+		echo "<span class=\"form-inline\">".$pages->display_jump_menu().$pages->display_items_per_page()."</span>";
+		echo '</div>';
+		echo '<div class="clearfix"></div>';
+		$limit	= $pages->limit_start.','.$pages->limit_end;
+		$qry 	=	$db->query($qryStr.' LIMIT '.$limit);
+	}
+	?>
+	<table class="table table-bordered table-striped table-hover">
+		<thead>
+			<tr class="bg-primary">
+				<th>Sr#</th>
+				<th>Date</th>
+				<th>DATA</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php 
+			if($count>0){
+				$n	=	1;
+				while($val	=	$qry->fetch_assoc()){ 
+			?>
+			<tr>
+				<td><?php echo $n++; ?></td>
+				<td><?php echo $val['dt']; ?></td>
+				<td><?php echo $val['DATA']; ?></td>
+			</tr>
+			<?php 
+				}
+			}else{?>
+			<tr>
+				<td colspan="6">No Record(s) Found!</td>
+			</tr>
+			<?php } ?>
+		</tbody>
+	</table>
+	<?php
+		echo '<div class="col-sm-6">';
+		echo '<nav aria-label="Page navigation"><ul class="pagination">';
+		echo $pages->display_pages();
+		echo '</ul></nav>';
+		echo '</div>';
+		echo '<div class="col-sm-6 text-right">';
+		echo "<p class=\"label label-default\">Page: $pages->current_page of $pages->num_pages</p>\n";
+		echo '</div>';
+		echo '<div class="clearfix"></div><hr>';
+		echo "<p class=\"code\">SELECT * FROM table LIMIT $pages->limit_start,$pages->limit_end (retrieve records $pages->limit_start-".($pages->limit_start+$pages->limit_end)." from table - $pages->total_items item total / $pages->items_per_page items per page)";
+	?>
+				
+	</div> <!--/.container-->
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
 </html>
