94d4f22bfa68fa0a30c152e606fcb473e018f016
Pratik Surve <prsurve@redhat.com>
2021-10-25 21:02:54 +0530

    Update retrieve-data.php

--- retrieve-data.php
+++ retrieve-data.php
@@ -1,5 +1,5 @@
 <?php
 include 'config.php';
-$query="select * from WORKLOAD ORDER BY srno DESC"; // Fetch all the data from the table WORKLOAD
+$query="select * from WORKLOAD ORDER BY srno DESC LIMIT 500"; // Fetch all the data from the table WORKLOAD
 $result=mysqli_query($db,$query);
 ?>
