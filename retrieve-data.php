52f2a81e774a1b2253ec3d7effad83d4e895ec1b
Pratik Surve <prsurve@redhat.com>
2021-10-25 11:42:49 +0530

    Show Data in DESC order

--- retrieve-data.php
+++ retrieve-data.php
@@ -1,5 +1,5 @@
 <?php
 include 'config.php';
-$query="select * from WORKLOAD"; // Fetch all the data from the table customers
+$query="select * from WORKLOAD ORDER BY dt DESC"; // Fetch all the data from the table WORKLOAD
 $result=mysqli_query($db,$query);
 ?>
