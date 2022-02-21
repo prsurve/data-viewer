f233c6574f692b2a67c076f9e4d157b9ddecae55
Pratik Surve <prsurve@redhat.com>
2022-02-21 17:24:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,11 @@
                 </tr>
                 </thead>
                 <tbody>
+                    <?php
+   $url=$_SERVER['REQUEST_URI'];
+   header("Refresh: 10; URL=$url");
+?>
+
                 <?php include 'retrieve-data.php'; ?>
                 <?php
                     if($result->num_rows > 0){
